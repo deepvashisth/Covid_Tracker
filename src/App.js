e512d19b9cc59c0bd80bbf6ccfc8765b1f602711
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-05-20 15:59:10 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './component/NavigationBar'
 import React from 'react'
+import Footer from './component/Footer'
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      
+      <Footer />
     </div>
   )
 }
--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -10,7 +10,7 @@ const NavigationBar = () => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <div className="collapse navbar-collapse " id="navbarTogglerDemo02">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-items">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
