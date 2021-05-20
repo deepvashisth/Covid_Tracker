e6562656990e660482147f211576eb99e094d2d7
deepvashisth <deepvashisth7777@gmail.com>
2021-05-21 00:45:15 +0530

    home fixed

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,33 +3,36 @@ import corrona from '../images/corrona.jpg'
 
 const Home = () => {
     return (
-       
-        
-            <div className='col'>
-                    <div className='row'>
+
+
+            <div className='container-fluid'>
+                    <div className='row rowColor'>
                         <div className='col-lg-6 col-md-6 left-box'>
-                        
+                            <br />
+                            <br />
+                            <br />
+                            <br />
                             <h1>Covid-19</h1>
                             <br></br>
                             <h3>Lets get vaccinated together</h3>
                             <br></br>
                             <br></br>
-                            <button type="button" class="btn btn-dark btn-lg download-button"> 
+                            <button type="button" class="btn btn-dark btn-lg download-button">
                             check Avaibility    <i class="fas fa-clinic-medical"></i>  </button>
                         </div>
 
                         <div className='col-lg-6 col-md-6 right-box'>
                         <img className='image_corrona' src={corrona} alt="" />
                         </div>
-                        
+
                     </div>
             </div>
 
-               
-                
-            
-    
-        
+
+
+
+
+
     )
 }
 
