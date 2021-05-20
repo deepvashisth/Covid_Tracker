104f84f67ef654d554ff6ce49674289d617e28c6
deepvashisth <deepvashisth7777@gmail.com>
2021-05-20 15:57:16 +0530

    Carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './component/NavigationBar'
 import React from 'react'
+import Carousel from './component/Carousel'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Carousel />
     </div>
   )
 }
