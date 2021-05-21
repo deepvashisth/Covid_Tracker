b28893e8918604a527f070fa74d7dc9a17382bed
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-05-21 13:20:49 +0530

    Routing Fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import Navbar from './component/NavigationBar'
 import React from 'react'
-import Footer from './component/Footer'
-import Home from './component/Home'
-import  {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
+import  {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import HomeComp from './component/HomeComp'
 import NeedHelp from './component/NeedHelp'
-import Carousel from './component/Carousel'
+
 
 const App = () => {
   return (
 
     <Router>
-      <div>
-        <Navbar />
-        
-        <Route path="/needhelp"  component={NeedHelp}/ >
-        <Home />
-        <Carousel />
-        <Footer />
-      
-      </div>
-
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={HomeComp} />
+              <Route path="/needhelp"  component={NeedHelp} />
+          
+          </Switch>
+          
+          
     </Router>
   )
 }
---
+++ src/component/HomeComp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './Home'
+import Footer from './Footer'
+import Carousel from './Carousel'
+
+const HomeComp = () => {
+    return (
+        <div>
+        <Home />
+        <Carousel />
+        <Footer  />
+
+        
+        </div>
+    )
+}
+
+export default HomeComp
