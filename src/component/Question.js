595ae78aa6123acff65a5184b6e88645e7d9569d
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-05-24 15:05:19 +0530

    Quiz fixed

---
+++ src/component/Question.js
@@ -0,0 +1,158 @@
+const questions = [
+    {
+                questionText: 'What is Covid-19 (2019 novel coronavirus)?',
+                answerOptions: [
+                    { answerText: 'A virus not previously seen in humans', isCorrect: false },
+                    { answerText: 'A virus first noted in China', isCorrect: false },
+                    { answerText: 'A virus that causes respiratory illness', isCorrect: false},
+                    { answerText: 'All of the above', isCorrect: true },
+                ],
+            },
+
+            {
+                questionText: 'What are the common symptoms of COVID-19? ',
+                answerOptions: [
+                    { answerText: 'A new and continuous cough', isCorrect: false },
+                    { answerText: 'Fever', isCorrect: false },
+                    { answerText: 'Tiredness', isCorrect: false},
+                    { answerText: 'All of the above', isCorrect: true },
+                ],
+            },
+
+
+            {
+                questionText: 'How is COVID-19 transmitted?',
+                answerOptions: [
+                    { answerText: 'Through blood', isCorrect: false },
+                    { answerText: 'Though contaminated food', isCorrect: false },
+                    { answerText: 'Through respiratory droplets in the air', isCorrect: true },
+                    { answerText: 'Both A & B', isCorrect: false },
+                ],
+            },
+    {
+                questionText: 'How does washing hands help prevent the spread of COVID-19?',
+                answerOptions: [
+                    { answerText: 'Soap rinses the virus off', isCorrect: false },
+                    { answerText: 'Soap destroys the virus', isCorrect: true },
+                    { answerText: 'Soap mutates the virus', isCorrect: false },
+                    { answerText: 'All of the above', isCorrect: false },
+                ],
+            },
+    
+            
+                 
+    {
+                questionText: 'Why should I avoid touching my face?',
+                answerOptions: [
+                    { answerText: 'COVID-19 enters the body through the mouth, nose, eyes', isCorrect: false },
+                    { answerText: 'Things you touch with your hands may have active virus on them', isCorrect: false },
+                    { answerText: 'The virus is absorbed easily by the pores on the face', isCorrect: false },
+                    { answerText: 'Both A & B', isCorrect: true },
+                ],
+            },
+    
+    
+    {
+                questionText: 'What is "social distancing”?',
+                answerOptions: [
+                    { answerText: 'Avoiding mass gatherings', isCorrect: false },
+                    { answerText: 'Staying at least 6 feet (2 meters) from others', isCorrect: false },
+                    { answerText: 'Working from home', isCorrect: false },
+                    { answerText: 'All of the above', isCorrect: true },
+                ],
+            },
+    
+    
+    {
+                questionText: 'How can I prevent spreading COVID-19 to others?',
+                answerOptions: [
+                    { answerText: 'Wash your hands.', isCorrect: false },
+                    { answerText: 'Avoid people if you are sick.', isCorrect: false },
+                    { answerText: 'Keep a distance of at least 6 feet.', isCorrect: false },
+                    { answerText: 'All of the above', isCorrect:true },
+                ],
+            },
+    
+    
+    {
+                questionText: 'Can you always tell if someone has COVID-19?',
+                answerOptions: [
+                    { answerText: 'No – not everyone with COVID-19 has symptoms', isCorrect: true },
+                    { answerText: 'Yes – it will be obvious, a person with COVID-19 coughs a lot', isCorrect: false },
+                    { answerText: 'Yes – you can tell just by where a person comes from, their race and ethnicity', isCorrect: false },
+                    { answerText: 'All of the above', isCorrect: false },
+                ],
+            },
+   
+
+
+    
+    {
+                questionText: 'Which of the following people is COVID-19 more dangerous for? (select all correct responses)',
+                answerOptions: [
+                    { answerText: 'Children', isCorrect: false },
+                    { answerText: 'Older people – especially those aged 70 and above', isCorrect: false },
+                    { answerText: 'People with certain underlying health conditions', isCorrect: false },
+                    { answerText: 'B & C both', isCorrect: true },
+                ],
+            },
+    
+    
+    
+    
+    {
+                questionText: 'Which of the following is an example of Social distancing? ',
+                answerOptions: [
+                    { answerText: 'You stop going to crowded places and visiting other people’s houses', isCorrect: true },
+                    { answerText: 'You stop talking to the people you live with', isCorrect: false },
+                    { answerText: 'You stop speaking to your friends on the phone', isCorrect: false },
+                    { answerText: 'All of the above', isCorrect: false },
+                ],
+            },
+    
+    
+    
+    
+    
+    {
+                questionText: 'Which of the following diseases are related to coronavirus?',
+                answerOptions: [
+                    { answerText: 'MERS', isCorrect: false },
+                    { answerText: 'SARS', isCorrect: false },
+                    { answerText: 'Neither A or B', isCorrect: false },
+                    { answerText: 'bothe A and B', isCorrect: true },
+                ],
+            },
+
+            
+
+            {
+                questionText: 'What are the Vaccines available in India',
+                answerOptions: [
+                    { answerText: 'Covisheild', isCorrect: false },
+                    { answerText: 'Covaxin', isCorrect: false },
+                    { answerText: 'Sputnik', isCorrect: false },
+                    { answerText: 'All of the Above', isCorrect: true },
+                ],
+            },
+
+            {
+                questionText: 'The COVID-19 vaccines are safe?',
+                answerOptions: [
+                    { answerText: 'Yes', isCorrect: true },
+                    { answerText: 'No', isCorrect: false }
+                ],
+            },
+
+            {
+                questionText: 'Are vaccines side effects normal??',
+                answerOptions: [
+                    { answerText: 'Yes', isCorrect: true },
+                    { answerText: 'No', isCorrect: false }
+                ],
+            },
+    
+    
+        ];
+
+export default questions;
