758006f70beab06f6fc095fb36733b490e4048bd
deepvashisth <deepvashisth7777@gmail.com>
2021-05-24 11:18:10 +0530

    quiz testing done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
-  
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-  
+
+app.get('/QuizHelp',function(req,res){
+  res.sendFile(__dirname + "src/component/Quiz.js");
+});
+
 const PORT = process.env.PORT || 5000;
-  
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
