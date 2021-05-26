ff5198125d77847aecd886c1e3ba7e4f26338f6d
deepvashisth <deepvashisth7777@gmail.com>
2021-05-26 16:38:37 +0530

    bug fixed

--- src/component/VaccineResult.js
+++ src/component/VaccineResult.js
@@ -4,13 +4,13 @@ import React from 'react'
 const VaccineResult = ({responseData,text}) => {
     return (
       <>
-            {   
-                
+            {
+
                 console.log(responseData.sessions)
                 
             }
             <h1>{text}</h1>
-    
+
         </>
     )
 }
