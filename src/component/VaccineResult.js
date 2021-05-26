03fb53cfaab969e39012807e98efabcc70cb06e1
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-05-26 06:23:13 +0530

    vaccineresult comp added

---
+++ src/component/VaccineResult.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const VaccineResult = ({responseData,text}) => {
+    return (
+      <>
+            {   
+                
+                console.log(responseData.sessions)
+                
+            }
+            <h1>{text}</h1>
+    
+        </>
+    )
+}
+
+export default VaccineResult
