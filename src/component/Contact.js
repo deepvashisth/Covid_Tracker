c369d8136f51dab8ac4e46669ce6ff64894f3dd7
deepvashisth <deepvashisth7777@gmail.com>
2021-05-27 00:20:24 +0530

    Contact filled

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -27,7 +27,13 @@ const Contact = () => {
             <img src={deep} className="card-img-top" alt="..."/>
             <div style={cardStlyling} className="card-body">
               <h5 className="card-title">Deep Vashisth</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text">
+                <div>
+                <a href='mailto:deepvashisth7777@gmail.com'>Gmail <i class="fas fa-envelope"></i></a><br />
+                <a href='https://www.linkedin.com/in/deep-vashisth-a71365154/' target="_blank" rel="noreferrer noopener">Linkedin <i class="fab fa-linkedin"></i></a><br />
+                <a href='https://github.com/deepvashisth' target="_blank" rel="noreferrer noopener">Github <i class="fab fa-github"></i></a>
+              </div>
+            </p>
             </div>
           </div>
         </div>
@@ -36,7 +42,13 @@ const Contact = () => {
             <img src={avadhesh} className="card-img-top" alt="..."/>
             <div style={cardStlyling} className="card-body">
               <h5 className="card-title">Avadhesh kr. singh</h5>
-              <p className="card-text">This is a short card.</p>
+              <p className="card-text">
+                <div>
+                <a href='mailto:kumarsinghavadhesh51@gmail.com'>Gmail <i class="fas fa-envelope"></i></a><br />
+                <a href='https://www.linkedin.com/in/avadhesh-singh-9577011a7/' target="_blank" rel="noreferrer noopener">Linkedin <i class="fab fa-linkedin"></i></a><br />
+                <a href='https://github.com/Ava68' target="_blank" rel="noreferrer noopener">Github <i class="fab fa-github"></i></a>
+              </div>
+              </p>
             </div>
           </div>
         </div>
@@ -45,7 +57,13 @@ const Contact = () => {
             <img src={ashish} className="card-img-top" alt="..."/>
             <div style={cardStlyling} className="card-body">
               <h5 className="card-title">Ashish Chauhan</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              <p className="card-text">
+                <div>
+                <a href='mailto:ashishrk2308@gmail.com'>Gmail <i class="fas fa-envelope"></i></a><br />
+                <a href='https://www.linkedin.com/in/ashish-chauhan-9bb847175' target="_blank" rel="noreferrer noopener">Linkedin <i class="fab fa-linkedin"></i></a><br />
+                <a href='https://github.com/ashishrk' target="_blank" rel="noreferrer noopener">Github <i class="fab fa-github"></i></a>
+              </div>
+            </p>
             </div>
           </div>
         </div>
@@ -53,8 +71,14 @@ const Contact = () => {
           <div className="card h-100">
             <img src={deep} className="card-img-top" alt="..."/>
             <div style={cardStlyling} className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title">Haribabu T</h5>
+              <p className="card-text card_d">
+                <div>
+                <a href='mailto:itisbabu1098@gmail.com'>Gmail <i class="fas fa-envelope"></i></a><br />
+                <a href='https://www.linkedin.com/in/haribabu-t-826301211' target="_blank" rel="noreferrer noopener">Linkedin <i class="fab fa-linkedin"></i></a><br />
+                <a href='/' target="_blank" rel="noreferrer noopener">Github <i class="fab fa-github"></i></a>
+              </div>
+              </p>
             </div>
           </div>
 
@@ -63,7 +87,10 @@ const Contact = () => {
       </div>
 
     </div>
-
+    <div className='foot'>
+      <a href='https://github.com/deepvashisth/Covid_Tracker' target="_blank" rel="noreferrer noopener">
+        You can fork this project on GitHub <i class="fab fa-github" target="_blank" rel="noreferrer noopener"></i></a>
+  </div>
   </div>)
 }
 
